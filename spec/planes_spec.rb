d0cb5cd34363ef298651f869edf53b5e3828022c
babelberry <tatienantes@yahoo.fr>
2014-08-25 12:05:25 +0100

    finalised plane

--- spec/planes_spec.rb
+++ spec/planes_spec.rb
@@ -8,12 +8,22 @@ describe Plane do
 		expect(plane.status?).to eq "flying"
 	end
 
+	it "has a flying status when up in the air" do
+		plane.take_off
+		expect(plane.status?).to eq "flying"
+	end
+
 	it "can land" do
 		plane.land
 		expect(plane.status?).to eq "landed"
 	end
 
 	it "can take off" do
+		plane.take_off
+		expect(plane.status?).to eq "flying"
+	end
+	
+	it "changes its status to flying after taking off" do
 		plane.land
 		plane.take_off
 		expect(plane.status?).to eq "flying"
